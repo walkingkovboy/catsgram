fd7fc4f35e27df5c8cab5af707335bb52f927275
Александр <walkingkowboy@gmail.com>
2025-07-28 15:02:10 +0300

    новые правки

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
-public class  gitMain {
+import ru.yandex.practicum.catsgram.model.User;
+
+public class  Main {
     public static void main(String[] args) {
+        User u = new User();
+        u.setUsername("Алекс");
+        System.out.println(u.getUsername());
+
         System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/ru/yandex/practicum/catsgram/model/User.java
@@ -0,0 +1,16 @@
+    package ru.yandex.practicum.catsgram.model;
+
+    import lombok.Data;
+    import lombok.EqualsAndHashCode;
+
+    import java.time.Instant;
+
+    @Data
+    @EqualsAndHashCode(of = {"email"})
+    public class User {
+        Long id;
+        String username;
+        String email;
+        String password;
+        Instant registrationDate;
+    }
